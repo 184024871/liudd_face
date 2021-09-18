b54242ae84706b014b237effe6df560fd1d1b302
184024871 <184024871@qq.com>
2021-09-18 08:49:00 +0800

    1

--- pages/submit/submit.vue
+++ pages/submit/submit.vue
@@ -1,9 +1,59 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<form @submit="formSubmit" @reset="formReset">
+			<view class="uni-form-item">
+				
+				<view class="title"><h2>我要上报</h2></view>
+				<br/>
+				<form @submit="formSubmit" @reset="formReset">
+					<view class="title">1.姓名</view>
+					<input value="王学彬" disabled="true" />
+					<br/>
+					<view class="title">2.地址</view>
+					<input value="寿县瓦埠镇西街666号" disabled="true" />
+					<br/>
+					<view class="uni-form-item uni-column">
+						<view class="title">3.是否损坏</view>
+						<radio-group name="radio">
+							<label>
+								<radio value="radio1" /><text>是</text>
+							</label>
+							<label>
+								<radio value="radio2" checked="ture"/><text>否</text>
+							</label>
+						</radio-group>
+					</view>
+					<br/>
+					<view class="uni-form-item uni-column">
+						<view class="title">4.是否堵水</view>
+						<radio-group name="radio">
+							<label>
+								<radio value="radio1" /><text>是</text>
+							</label>
+							<label>
+								<radio value="radio2" checked="ture"/><text>否</text>
+							</label>
+						</radio-group>
+					</view>
+					<br/>
+					<view class="uni-form-item uni-column">
+						<view class="title">5.是否清掏</view>
+						<radio-group name="radio">
+							<label>
+								<radio value="radio1" /><text>是</text>
+							</label>
+							<label>
+								<radio value="radio2" checked="ture"/><text>否</text>
+							</label>
+						</radio-group>
+					</view>
+					<br/>
+					<view class="uni-btn-v">
+						<button form-type="submit">提交</button>
+					</view>
+				</form>
+			</view>
+		</form>
 	</view>
 </template>
 
